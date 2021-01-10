584d337ee44572dbe718df67b65beb6188238ea0
Pravas Chinthoju <pravasith@gmail.com>
2021-01-10 20:14:58 +0530

    looking for a beautiful change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,19 @@ import '../src/assets/scss/libs/global.scss'
 import Layout from '../src/components/layout/index'
 
 
+import Router from 'next/router'
+import NProgress from 'nprogress' //nprogress module
+import 'nprogress/nprogress.css' //styles of nprogress
+
+
+NProgress.configure({ showSpinner: false })
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
+
 const App= ({ Component, pageProps, router }: AppProps) => {
 
     
