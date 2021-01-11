f640f502cdd1554ba68551940eba7f014617086b
Pravas Chinthoju <pravasith@gmail.com>
2021-01-11 20:32:32 +0530

    Looking for a beautiful change

--- src/library/apollo.ts
+++ src/library/apollo.ts
@@ -1,9 +1,9 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client"
 
-const uri = 'http://localhost:4000/graphql'
+// const uri = 'http://localhost:4000/graphql'
 
 // const uri = 'https://spotifinder-backend1.herokuapp.com/graphql'
-// const uri = 'https://api-spotifinder.pravasdesign.com/graphql'
+const uri = 'https://api-spotifinder.pravasdesign.com/graphql'
 
 
 const link = createHttpLink({
