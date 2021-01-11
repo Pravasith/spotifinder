1dda00a7be7e6542c14b63737f9c3e3da34a18fd
Pravas Chinthoju <pravasith@gmail.com>
2021-01-11 11:46:04 +0530

    looking for a beautiful change

--- pages/artist/[id].tsx
+++ pages/artist/[id].tsx
@@ -100,7 +100,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     return {
         // Set the timeout for generating to 1 second
         // This timeout could be longer depending on how often data changes
-        revalidate: 1,
+        revalidate: 10,
         props : {
             artistData
         }
